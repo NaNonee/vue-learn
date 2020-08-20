350d70bfaf761bd71a4049af4a5cfaadb460ae9d
wanght <kihtoner@163.com>
2020-08-20 23:59:46 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import echarts from 'echarts'
+import '@/assets/css/global.css' //导入全局样式表
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
+Vue.prototype.$echarts = echarts
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Login from '@/views/Login.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',//去掉#
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path:'/login',component:Login },
+    {path:'/',redirect:'/login'}
+
   ]
 })
