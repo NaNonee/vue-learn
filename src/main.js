8c80cd42dc96f1a6cb1a175b2c8f77abde13e356
wanght <kihtoner@163.com>
2020-08-22 22:49:55 +0800

    第二次提交

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
 import echarts from 'echarts'
+import './plugins/element.js'
+import axios from 'axios'
 import '@/assets/css/global.css' //导入全局样式表
+import 'element-ui/lib/theme-chalk/index.css' //Elment-ui的配置
+// 导入字体图标
+import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+// 挂在到Vue实例，后面可通过this调用
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 Vue.prototype.$echarts = echarts
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/views/Login.vue'
+import Home from '@/views/Home.vue'
 Vue.use(Router)
-
-export default new Router({
+const routes = [
+  {path:'/login',component:Login },
+    {path:'/',redirect:'/login'},
+    {path:'/home',component:Home}
+]
+const router = new Router({
   mode:'history',//去掉#
-  routes: [
-    {path:'/login',component:Login },
-    {path:'/',redirect:'/login'}
-
-  ]
+  routes
 })
+// 挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作 next('/login')强制跳转login
+router.beforeEach((to, from, next) => {
+  // 访问登录页，放行
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 没有token, 强制跳转到登录页
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+
+export default router
